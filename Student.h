45c124e2df92ea4516e383f996713c2a085d4a99
afanofmyself <73419089+afanofmyself@users.noreply.github.com>
2022-07-18 20:01:07 +0800

    Add files via upload
    作业2

---
+++ Student.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+@interface Student : NSObject{
+	NSString *_name;
+	NSString *_major;
+	NSInteger age;
+}
+
+- (void)printTime:(float)time;
+@end
---
+++ Student.m
@@ -0,0 +1,10 @@
+#import "Student.h"
+@implementation Student
+- (void)printTime:(float)time{
+	NSLog(@"time:%f",time);
+}
+@end
+int main(int argc,const * argv[]){
+	
+	return 0;
+}
