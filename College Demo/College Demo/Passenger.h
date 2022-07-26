3f3770c3d421862cc49024beb3f213b34d22d0ed
afanofmyself <73419089+afanofmyself@users.noreply.github.com>
2022-07-26 19:02:13 +0800

    Update Passenger.h

--- College Demo/College Demo/Passenger.h
+++ College Demo/College Demo/Passenger.h
@@ -18,11 +18,14 @@ NS_ASSUME_NONNULL_BEGIN
 // 是否年满 18 岁
 // 历史订单 （数组）
 // 未出行订单 （数组）
-
+@property (nonatomic,copy) BOOL *eighteen;
+@property (nonatomic,copy) NSMutableArray * usedOrder = [[NSMutableArray alloc]init];
+@property (nonatomic,copy) NSMutableArray * unusedOrder = [[NSMutableArray alloc]init];
 // Function 方法
 // 去订票
-
+- (BOOL) toBookTicket:(order *)ticket;
 // 去检票
+- (BOOL) toCheckTicket:(Order *)ticket;
 @end
 
 NS_ASSUME_NONNULL_END
